fd510453669e5c166e1428b4f58b60460284e8e5
Kevin Calopisis <kevincal@umich.edu>
2025-12-01 17:25:23 -0500

    alex is not cooking

--- src/day_1.go
+++ src/day_1.go
@@ -48,19 +48,29 @@ func day1Part2(filename string) {
 		if len(lines[i]) == 0 {
 			continue
 		}
-		left := lines[i][0] == 'L'
-		a, _ := strconv.Atoi(lines[i][1:])
-		for range a {
-			if left {
-				curr -= 1
-			} else {
-				curr += 1
-			}
-			if curr%100 == 0 {
-				cnt += 1
-			}
 
+		left := lines[i][0] == 'L'
+		difference, _ := strconv.Atoi(lines[i][1:])
+		prev := curr
+
+		// left and negative: should help you by the -x mod 100
+		// left and positive: should help you by the -x mod 100
+		// right and positive: should help you by the x mod 100
+		// right and negative: should not help you by the x mod 100
+
+		offset := 0
+		if left {
+			offset = (-prev%100 + 100) % 100
+			curr -= difference
+		} else {
+			offset = (prev%100 + 100) % 100
+			curr += difference
 		}
+
+		difference += offset
+
+		cnt += difference / 100
+
 	}
 	fmt.Printf("Answer to day 1 part 2: %d\n", cnt)
 }
