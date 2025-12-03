1a97d4b6a27d5fcef5991108d42980876ced8e81
Kevin Calopisis <kevincal@umich.edu>
2025-12-02 23:44:06 -0500

    days

---
+++ src/day_11/go.mod
@@ -0,0 +1,3 @@
+module github.com/kevincal1226/advent-of-code-2025
+
+go 1.25.1
---
+++ src/day_4/day.go
@@ -0,0 +1,14 @@
+package main
+
+import "os"
+
+func part1(filename string) {
+}
+
+func part2(filename string) {
+}
+
+func main() {
+	part1(os.Args[1])
+	part2(os.Args[1])
+}
