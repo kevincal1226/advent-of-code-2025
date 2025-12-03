848f7c9b9e5884ddb46ef7b2f4501167c1ff4a67
Kevin Calopisis <kevincal@umich.edu>
2025-12-03 00:26:37 -0500

    day 3 monotonic stack?? :skull:

--- src/day_3/day.go
+++ src/day_3/day.go
@@ -1,11 +1,73 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func part1(filename string) {
+	f, _ := os.Open(filename)
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	sum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		biggestBoy, _ := strconv.Atoi(line[0:1])
+		best := 0
+		for i, c := range line {
+			if i == 0 {
+				continue
+			}
+			s, _ := strconv.Atoi(string(c))
+			best = max(best, biggestBoy*10+s)
+			if biggestBoy < s {
+				biggestBoy = s
+			}
+		}
+		sum += best
+	}
+
+	fmt.Println(sum)
 }
 
 func part2(filename string) {
+	f, _ := os.Open(filename)
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	sum := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		stack := []int{}
+		lineLen := len(line)
+		for i, c := range line {
+			s, _ := strconv.Atoi(string(c))
+			for len(stack) > 0 && len(stack)+(lineLen-i-1) >= 12 && stack[len(stack)-1] < s {
+				stack = stack[0 : len(stack)-1]
+			}
+			if len(stack) < 12 {
+				stack = append(stack, s)
+			}
+		}
+		best := 0
+		for _, i := range stack {
+			best = best*10 + i
+		}
+		sum += best
+	}
+
+	fmt.Println(sum)
 }
 
 func main() {
